9897fdfa05596afdf4a09f4fd96af0b3b26c933c
fcoverdugoa <fcoverdugoa@underpost.net>
2020-11-23 20:55:38 -0400

    Add fondos

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-include 'C:/xampp/htdocs/nexodev/modules/path.php';
+include 'C:/xampp/htdocs/nexodev/modules/path0.php';
 $str_data = file_get_contents('C:/xampp/htdocs/nexodev/draft/json/underpost.json');
 $uri = $_SERVER['REQUEST_URI'];
 renderPath($uri, $str_data, 0);
