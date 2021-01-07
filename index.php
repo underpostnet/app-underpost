d62db42aa0ef55ec1f5e7af41b9b383a62cc529c
fcoverdugoa <fcoverdugoa@underpost.net>
2021-01-07 15:50:36 -0400

    update fw path

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-include 'C:/xampp/htdocs/nexodev/modules/underpost.php';
+include 'C:/dd/deploy_area/nexodev/modules/underpost.php';
 $str_data = file_get_contents('C:/dd/global_data/json/underpost/underpost.json');
 $uri = $_SERVER['REQUEST_URI'];
 renderPath($uri, $str_data, 0);
